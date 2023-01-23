04cbecec5b448d313d99e47dba09f97c85a9e569
Karen <karenlunafeldheim@gmail.com>
2023-01-23 02:50:47 -0300

    Seguimos aprendiendo

---
+++ pruebaconsola.js
@@ -0,0 +1,4 @@
+var Nombre= "Karen";
+Nombre= 40;
+console.log("Nombre vale" + Nombre);
+console.log("Nombre vale" + Nombre);
---
+++ sintaxis.js
@@ -0,0 +1,12 @@
+
+var miVariable;
+let nombre;
+const IVA = 21;
+
+/**
+ * Esto es un 
+ * comentario de 
+ * muchas líneas.
+ */
+
+//Comentario de una sola línea
